1270160c8eac79b484478afb3baf36bff65e08d4
asavin1 <a.savin1@g.nsu.ru>
2023-09-27 21:48:46 +0700

    final version

---
+++ Task_1_1_1/src/main/java/org/example/Main.java
@@ -0,0 +1,22 @@
+package org.example;
+import java.util.Scanner;
+
+
+public class Main {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        System.out.println("Insert size array:");
+        int size = input.nextInt();
+        int[] array = new int[size];
+        System.out.println("Insert array elements:");
+
+        for (int i = 0; i < size; i++) {
+            array[i] = input.nextInt(); //
+        }
+        HeapSort.sort(array);
+        System.out.println("Sorted array is");
+        for (int i = 0; i < size; ++i) {
+            System.out.print(array[i] + " ");
+        }
+    }
+}
--- Task_1_1_1/src/test/java/org/example/HeapSortTest.java
+++ Task_1_1_1/src/test/java/org/example/HeapSortTest.java
@@ -2,24 +2,58 @@ package org.example;
 
 import org.junit.jupiter.api.Test;
 
+
+import java.util.Arrays;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 
 public class HeapSortTest {
     @Test
-    void checkMain()  {
+    void test1()  {
         int[] arr = {4, 10, 3, 5, 1};
         int[] ans = {1, 3, 4, 5, 10};
-
-        HeapSort ob = new HeapSort();
-        ob.sort(arr);
-
+        HeapSort.sort(arr);
         assertArrayEquals(ans, arr);
     }
 
+    @Test
+    void test2()  {
+        int[] array = new int[100000];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = ((int)(Math.random() * 100000));
+        }
+        int[] ans = Arrays.copyOf(array, array.length);
+        HeapSort.sort(array);
+        Arrays.sort(ans);
+        assertArrayEquals(ans, array);
+    }
 
     @Test
-    void doINeedToCheckDefaultConstructor() {
-        final var sampleInstance = new HeapSort();
+    void test3()  {
+        int[] array = new int[100];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = ((int)(Math.random() * -100));
+        }
+        int[] ans = Arrays.copyOf(array, array.length);
+        HeapSort.sort(array);
+        Arrays.sort(ans);
+        assertArrayEquals(ans, array);
+    }
+
+    @Test
+    void test4()  {
+        int[] array = {1};
+        int[] ans = {1};
+        HeapSort.sort(array);
+        assertArrayEquals(ans, array);
+    }
+
+    @Test
+    void test5()  {
+        int[] array = {};
+        int[] ans = {};
+        HeapSort.sort(array);
+        assertArrayEquals(ans, array);
     }
 }
