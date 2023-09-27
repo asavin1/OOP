3405f381cbdb0e7a83a5f6622d2c09e72a5c9005
asavin1 <a.savin1@g.nsu.ru>
2023-09-27 21:52:57 +0700

    finish

--- Task_1_1_1/src/test/java/org/example/HeapSortTest.java
+++ Task_1_1_1/src/test/java/org/example/HeapSortTest.java
@@ -1,12 +1,8 @@
 package org.example;
 
-import org.junit.jupiter.api.Test;
-
-
 import java.util.Arrays;
-
-import static org.junit.jupiter.api.Assertions.*;
-
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
 public class HeapSortTest {
     @Test
