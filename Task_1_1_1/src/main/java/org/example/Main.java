55a2d879eb0f91f38b2b2ba38301610322e27023
Anatoliy Savin <a.savin1@g.nsu.ru>
2023-10-10 01:15:14 +0700

    1

--- Task_1_1_1/src/main/java/org/example/Main.java
+++ Task_1_1_1/src/main/java/org/example/Main.java
@@ -1,8 +1,25 @@
 package org.example;
 
+import java.util.Scanner;
+
+/**
+ * Точка входа
+ */
 public class Main {
     public static void main(String[] args) {
-        Polynomial p1 = new Polynomial(new int[] {4, 3, 6, 7});
-        System.out.println(p1);
+        Scanner input = new Scanner(System.in);
+        System.out.println("Insert size array:");
+        int size = input.nextInt();
+        int[] array = new int[size];
+        System.out.println("Insert array elements:");
+
+        for (int i = 0; i < size; i++) {
+            array[i] = input.nextInt(); //
+        }
+        HeapSort.sort(array);
+        System.out.println("Sorted array is");
+        for (int i = 0; i < size; ++i) {
+            System.out.print(array[i] + " ");
+        }
     }
 }
---
+++ Task_1_1_2/src/main/java/org/example/Main.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        Polynomial p1 = new Polynomial(new int[] {4, 3, 6, 7});
+        System.out.println(p1);
+    }
+}
