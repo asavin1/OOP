19958c4a45be9647f22bf2d73adb05612e33184d
Anatoliy Savin <a.savin1@g.nsu.ru>
2023-11-17 02:10:52 +0700

    123

---
+++ Task_1_3_1/src/main/java/org/example/findSubstringIndices.java
@@ -0,0 +1,55 @@
+package org.example;
+
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class findSubstringIndices {
+    public static ArrayList<Integer> find(String filename, String substr) {
+        var substring = new String(substr.getBytes(), StandardCharsets.UTF_8);
+
+        var result = new ArrayList<Integer>();
+        try {
+            FileInputStream fileIS = new FileInputStream(filename);
+            InputStreamReader inputSR = new InputStreamReader(fileIS, StandardCharsets.UTF_8);
+            BufferedReader bufferedReader = new BufferedReader(inputSR);
+
+            char[] buff = new char[substring.length()];
+            var a = bufferedReader.read(buff, 0, substring.length());
+            char[] temp;
+            int index = 0;
+
+            char[] sub = new char[substring.length()];
+            for (int i=0; i<substring.length(); i++) {
+                sub[i] = substring.charAt(i);
+            }
+
+            while (a != -1) {
+                if (Arrays.equals(buff, sub)) {
+                    result.add(index);
+                }
+                a = bufferedReader.read();
+                temp = buff.clone();
+                System.arraycopy(temp, 1, buff, 0, substring.length() - 1);
+                buff[substring.length() - 1] = (char) a;
+                index++;
+            }
+            bufferedReader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+
+    public static void main(String[] args) {
+        String filename = "input.txt";
+        String substring = "бра";
+        System.out.println(find("src/main/resources/" + filename, substring));
+    }
+}
---
+++ Task_1_3_1/src/test/java/org/example/findSubstringIndicesTest.java
@@ -0,0 +1,27 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import static org.example.findSubstringIndices.find;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class findSubstringIndicesTest {
+    @Test
+    void test1()  {
+        var filename = "src/test/resources/" + "test1.txt";
+        var substring = "бра";
+        var except = new ArrayList<>(Arrays.asList(1, 8));
+        assertEquals(find(filename, substring), except);
+    }
+
+    @Test
+    void test2()  {
+        var filename = "src/test/resources/" + "test2.txt";
+        var substring = "AAA";
+        var except = new ArrayList<>(Arrays.asList(0, 4, 5, 6, 7));
+        assertEquals(find(filename, substring), except);
+    }
+}
