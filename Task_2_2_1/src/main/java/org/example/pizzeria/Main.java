f42169d7cf420e4ea194ff0e4233d963a2c91a0d
Anatoliy Savin <a.savin1@g.nsu.ru>
2024-03-15 00:57:41 +0700

    1

--- Task_2_2_1/src/main/java/org/example/pizzeria/Main.java
+++ Task_2_2_1/src/main/java/org/example/pizzeria/Main.java
@@ -10,22 +10,15 @@ public class Main {
         Pizzeria pizzeria;
 
         try {
-            // Create a pizzeria instance with a working time of 20,000 milliseconds
             pizzeria = new Pizzeria(10000);
         } catch (IOException e) {
             System.out.println("Не удаётся найти файл");
             return;
         }
-
-        // Start the pizzeria simulation
         pizzeria.start();
-
-        // Add orders to the pizzeria
         for (int i = 1; i <= 10; i++) {
             pizzeria.addOrder(i);
         }
-
-        // Wait for the pizzeria simulation to complete
         pizzeria.join();
     }
 }
