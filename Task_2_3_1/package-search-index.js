0b9614b33a1cbcda9513474854bb57e1d0b4ea88
asavin1 <asavin1@users.noreply.github.com>
2024-03-25 20:39:29 +0000

    Deploying to gh-pages from @ asavin1/OOP@561dcb67457307cd6d0b143e2513cdf2600ab244 🚀

---
+++ Task_2_3_1/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"m":"com.example.snake","l":"com.example.snake"}]
---
+++ Task_2_3_1/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"com.example.snake","l":"SnakeGame.Cell"},{"p":"com.example.snake","l":"SnakeGame.Dir"},{"p":"com.example.snake","l":"HelloController"},{"p":"com.example.snake","l":"SnakeGame"}]
